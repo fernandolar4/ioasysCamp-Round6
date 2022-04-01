362ac8ed3b53de94bee43c305f4022ea86937e68
Fernando Lara <fmaia8@hotmail.com>
2022-04-01 17:21:32 -0300

    Pagina de Pacotes

--- src/pages/Pacotes/Pacotes.style.js
+++ src/pages/Pacotes/Pacotes.style.js
@@ -1,1 +1,41 @@
 import styled from "styled-components";
+
+export const Pacotes = styled.section`
+  padding: 35px 32px 20px;
+
+  h2 {
+    display: block;
+    font-size: 32px;
+    color: ${({ theme }) => theme.colors.neutralDarker};
+    font-weight: 600;
+    margin-bottom: 42px;
+  }
+  section {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    a {
+      text-decoration: none;
+      div {
+        box-sizing: border-box;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        img {
+          object-fit: cover;
+          height: 160px;
+          width: 140px;
+          margin-bottom: 20px;
+        }
+        span {
+          color: ${({ theme }) => theme.colors.neutralDarker};
+          font-size: 14px;
+          font-weight: 700;
+          margin-bottom: 50px;
+        }
+      }
+    }
+  }
+`;
--- src/pages/Pacotes/index.jsx
+++ src/pages/Pacotes/index.jsx
@@ -2,11 +2,26 @@ import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 import * as S from "./Pacotes.style";
 
+import Mock from "../../mock/pacotes.json";
+import { Link } from "react-router-dom";
+
 const Pacotes = () => {
   return (
     <>
       <NavBar />
-      <h1>PACOTES</h1>
+      <S.Pacotes>
+        <h2>Pacotes de Turismo</h2>
+        <section>
+          {Mock.pacotes.map((pacote) => (
+            <Link to="/viagem">
+              <div>
+                <img src={pacote.foto} alt="foto da localidade" />
+                <span>{pacote.nome}</span>
+              </div>
+            </Link>
+          ))}
+        </section>
+      </S.Pacotes>
       <Footer />
     </>
   );
