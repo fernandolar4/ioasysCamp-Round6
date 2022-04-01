5ddc9e98a3d7bc876926da300a1b9ab9e6e81cdd
Fernando Lara <fmaia8@hotmail.com>
2022-04-01 18:22:12 -0300

    Pagina Resumo

--- src/pages/Resumo/Resumo.style.js
+++ src/pages/Resumo/Resumo.style.js
@@ -1,1 +1,22 @@
 import styled from "styled-components";
+
+export const Resumo = styled.section`
+  padding: 30px 32px 20px;
+  min-height: 60vh;
+  h2 {
+    display: block;
+    font-size: 32px;
+    color: ${({ theme }) => theme.colors.neutralDarker};
+    font-weight: 400;
+    margin-bottom: 30px;
+  }
+  p {
+    font-size: 12px;
+    font-weight: 400;
+    margin-bottom: 40px;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+  }
+`;
--- src/pages/Resumo/index.jsx
+++ src/pages/Resumo/index.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/Button/Button.style";
 import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 import * as S from "./Resumo.style";
@@ -6,7 +7,17 @@ const Resumo = () => {
   return (
     <>
       <NavBar />
-      <h1>Resumo</h1>
+      <S.Resumo>
+        <h2>Sua compra foi efetuada com sucesso!</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringilla urna,
+          porttitor
+        </p>
+        <div>
+          <Button className="disabled">Conferir minhas viagens</Button>
+        </div>
+      </S.Resumo>
       <Footer />
     </>
   );
