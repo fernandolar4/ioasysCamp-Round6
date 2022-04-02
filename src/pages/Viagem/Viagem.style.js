89b54c41f91be3b9570f9d5ddd4bfede00fe2355
Fernando Lara <fmaia8@hotmail.com>
2022-04-02 13:25:29 -0300

    1/2 pagina viagem

--- src/pages/Viagem/Viagem.style.js
+++ src/pages/Viagem/Viagem.style.js
@@ -2,12 +2,49 @@ import styled from "styled-components";
 
 export const Viagem = styled.section`
   padding: 35px 32px 20px;
- 
+`;
+export const Info = styled.section`
   h2 {
     display: block;
     font-size: 32px;
     color: ${({ theme }) => theme.colors.neutralDarker};
     font-weight: 600;
     margin-bottom: 42px;
+  }
+`;
+
+export const Dicas = styled.section`
+  margin-top: 30px;
+
+  .iconName {
+    display: flex;
+    align-items: center;
+    margin-bottom: 12px;
+    img {
+      margin-right: 12px;
+    }
+    p {
+      font-size: 18px;
+      font-weight: 600;
+      color: ${({ theme }) => theme.colors.neutralDarker};
+    }
+  }
+
+  .containerDicas {
+    display: flex;
+    flex-direction: row;
+    .barraV {
+      width: 1px;
+      height: 64px;
+      background-color: ${({ theme }) => theme.colors.neutralMediumLight};
+      margin-left: 10px;
+      margin-right: 23px;
+      margin-bottom: 19px;
+    }
+    p {
+      font-size: 12px;
+      font-weight: 400;
+      color: ${({ theme }) => theme.colors.neutralDark};
+    }
   }
 `;
--- src/pages/Viagem/index.jsx
+++ src/pages/Viagem/index.jsx
@@ -6,20 +6,77 @@ import Mock from "../../mock/pacotes.json";
 import Button from "../../components/Button";
 import { Link } from "react-router-dom";
 
+import Calendar from "../../assets/icons/Calendar.svg";
+import Camping from "../../assets/icons/Camping.svg";
+import Mapa from "../../assets/icons/Map.svg";
+import Mountain from "../../assets/icons/Mountain.svg";
+
 const Viagem = () => {
   return (
     <>
       <NavBar />
       <S.Viagem>
-        <img src={Mock.pacotes[1].foto} alt="Foto do local" />
-        <div>
-          <span>{Mock.pacotes[1].nome}</span>
-          <span>R${Mock.pacotes[1].valor}</span>
-        </div>
-        <p>X vagas ainda disponíveis</p>
+        <S.Info>
+          <img src={Mock.pacotes[1].foto} alt="Foto do local" />
+          <div>
+            <span>{Mock.pacotes[1].nome}</span>
+            <span>R${Mock.pacotes[1].valor}</span>
+          </div>
+          <p>X vagas ainda disponíveis</p>
+        </S.Info>
         <Button className="secondary">
           <Link to="/resumo">Comprar Pacote</Link>
         </Button>
+        <S.Dicas>
+          <div className="iconName">
+            <img src={Camping} alt="Icone" />
+            <p>Hospedagem</p>
+          </div>
+          <div className="containerDicas">
+            <div className="barraV" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor
+            </p>
+          </div>
+          <div className="iconName">
+            <img src={Mountain} alt="Icone" />
+            <p>Hospedagem</p>
+          </div>
+          <div className="containerDicas">
+            <div className="barraV" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor
+            </p>
+          </div>
+          <div className="iconName">
+            <img src={Calendar} alt="Icone" />
+            <p>Hospedagem</p>
+          </div>
+          <div className="containerDicas">
+            <div className="barraV" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor
+            </p>
+          </div>
+          <div className="iconName">
+            <img src={Mapa} alt="Icone" />
+            <p>Hospedagem</p>
+          </div>
+          <div className="containerDicas">
+            <div className="barraV" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor
+            </p>
+          </div>
+        </S.Dicas>
       </S.Viagem>
       <Footer />
     </>
