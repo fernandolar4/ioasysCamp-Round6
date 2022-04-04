03e72c73185437d36e2c8bf8472873178be68120
Fernando Lara <fmaia8@hotmail.com>
2022-04-04 18:23:46 -0300

    Nav Responsiva e Offcanvas

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,46 +1,43 @@
 import * as S from "./NavBar.style";
 
-import Hamburguer from "../../assets/icons/Hamburguer.svg";
+import Carrinho from "../../assets/icons/Cart.svg";
+
 import { Link } from "react-router-dom";
-import {
-  Container,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
-import { Offcanvas } from "bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar bg="light" expand="false" sticky="top">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Nav.Link>
+          <img src={Carrinho} alt="Carrinho" />
+        </Nav.Link>
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel"></Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <S.NavStyles>
+                <Nav.Link id="linksOffscreen" className="teste" href="/pacotes">
+                  Sobre nós
+                </Nav.Link>
+                <Nav.Link id="linksOffscreen" href="#action2">
+                  Pacotes turísticos
+                </Nav.Link>
+                <Nav.Link id="linksOffscreen">Pacotes turísticos</Nav.Link>
+                <Nav.Link id="linksOffscreen">Minha conta</Nav.Link>
+              </S.NavStyles>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
--- src/components/NavBar/NavBar.style.js
+++ src/components/NavBar/NavBar.style.js
@@ -1,96 +1,19 @@
 import styled from "styled-components";
 
-export const Navbar = styled.nav`
-  box-sizing: border-box;
-  background: ${({ theme }) => theme.colors.neutralLighter};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 56px;
-  padding: 22px 33px 22px;
+export const NavStyles = styled.div`
+  *,
+  *::after,
+  *::before {
+    font-family: "Outfit", sans-serif;
+  }
+  #linksOffscreen {
+    color: ${({ theme }) => theme.colors.neutralDarker};
+    padding-left: 32px;
+    margin-bottom: 40px;
 
-  position: fixed;
-  top: 0;
-  width: 100%;
-
-  ul {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 39px;
-    li {
-      color: black;
-      text-decoration: none;
-      list-style: none;
+    &:hover {
+      color: ${({ theme }) => theme.colors.primary};
+      text-decoration: underline;
     }
   }
-
-${
-  "" /* 
- ARRUMAR O HAMBurguer NÃO ESTÁ FUNCIONAL DESSA FORMA */
-}
-
-  .hamburguer {
-    display: none;
-    .bar {
-      display: block;
-      width: 25px;
-      height: 3px;
-      margin: 5px auto;
-      -webkit-transition: all 0.3s ease-in-out;
-      transition: all 0.3s ease-in-out;
-      background-color: black;
-    }
-    }
-  }
-
-  @media(max-width:768px){
-      .hamburguer {
-          display: block;
-      }
-      .hamburguer.active .bar:nth-child(2){
-          opacity: 0;
-      }
-      .hamburguer.active .bar:nth-child(1){
-          transform: translateY(8px) rotate(45deg)
-      }
-      .hamburguer.active .bar:nth-child(3){
-          transform: translateY(-8px) rotate(-45deg)
-      }
-
-      ul{
-        display: none;
-          top: 700px;
-          gap: 0;
-          flex-direction: column;
-          width: 50%;
-          text-align: center;
-          transition: 0.3s;
-
-          &:active{
-            left: 0;
-          }
-  }
-
-  }
-
-  a {
-    background-color: transparent;
-    position: relative;
-    text-decoration: none;
-    color: black;
-
-    &:after {
-      content: "";
-      padding: 24px;
-      position: absolute;
-      left: -15px;
-      top: -15px;
-    }
-  }
-`;
-
-export const Espace = styled.nav`
-  display: block;
-  margin-bottom: 56px;
 `;
