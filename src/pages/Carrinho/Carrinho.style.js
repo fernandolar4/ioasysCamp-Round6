57fc822d3a94d8b5ac7fd998a85b8cb69116e364
Fernando Lara <fmaia8@hotmail.com>
2022-04-18 13:04:12 -0300

    .

--- src/pages/Carrinho/Carrinho.style.js
+++ src/pages/Carrinho/Carrinho.style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import pattern from "../../assets/pattern/pattern.svg";
+import bgD from "../../assets/pattern/backgroundDesktop.svg";
 
 export const Carrinho = styled.main`
   padding: 40px 32px 40px;
@@ -7,6 +8,12 @@ export const Carrinho = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  @media (min-width: 768px) {
+    overflow-x: hidden;
+    background-image: url(${bgD});
+    background-position: 1%;
+    background-repeat: repeat-y;
+  }
 
   .containerBgB {
     position: absolute;
@@ -15,6 +22,9 @@ export const Carrinho = styled.main`
     background-image: url(${pattern});
     height: 40px;
     width: 55%;
+    @media (min-width: 768px) {
+      background-image: none;
+    }
   }
 
   h2 {
@@ -24,6 +34,12 @@ export const Carrinho = styled.main`
     line-height: 120%;
     color: ${({ theme }) => theme.colors.neutralDarker};
     margin-bottom: 40px;
+    @media (min-width: 768px) {
+      font-weight: 600;
+      font-size: 40px;
+      line-height: 120%;
+      margin-left: 10vw;
+    }
   }
 
   .carrinhoCard {
@@ -31,6 +47,43 @@ export const Carrinho = styled.main`
     background: ${({ theme }) => theme.colors.neutralLight};
     box-shadow: 0px 2px 10px rgba(37, 42, 39, 0.2);
     border-radius: 20px;
+    @media (min-width: 768px) {
+      width: 80vw;
+      margin: 0 auto;
+      display: flex;
+    }
+
+    .imgPacote {
+      display: none;
+      @media (min-width: 768px) {
+        display: block;
+        width: 216px;
+        height: 216px;
+        box-shadow: 0px 2px 35px rgba(37, 42, 39, 0.1);
+        border-radius: 20px;
+      }
+    }
+
+    .carrinhoSection {
+      @media (min-width: 768px) {
+        margin-left: 60px;
+        img {
+          display: none;
+        }
+        .carrinhoNome {
+          font-weight: 600;
+          font-size: 32px;
+          line-height: 120%;
+          .carrinhoTitulo {
+            p {
+              font-weight: 600;
+              font-size: 24px;
+              line-height: 120%;
+            }
+          }
+        }
+      }
+    }
     .carrinhoNome {
       display: flex;
       flex-direction: row;
--- src/pages/Carrinho/index.jsx
+++ src/pages/Carrinho/index.jsx
@@ -51,77 +51,84 @@ const Carrinho = () => {
           <div className="containerBgB"></div>
           <h2>Carrinho de compras</h2>
           <section className="carrinhoCard">
-            <div className="carrinhoNome">
-              <div className="carrinhoTitulo">
-                <img src={Camping} alt="Icone" />
-                <p> {viagem.tourName}</p>
+            <img
+              src={viagem.photo1}
+              alt={viagem.description}
+              className="imgPacote"
+            ></img>
+            <section className="carrinhoSection">
+              <div className="carrinhoNome">
+                <div className="carrinhoTitulo">
+                  <img src={Camping} alt="Icone" />
+                  <p> {viagem.tourName}</p>
+                </div>
+                <p>R$ {viagem.price * pessoas}</p>
               </div>
-              <p>R$ {viagem.price * pessoas}</p>
-            </div>
-            <div className="carrinhoPessoas">
-              <p>Quantidade de pessoas</p>
-              <div>
-                <button
-                  onClick={(e) => {
-                    minusPessoa();
-                  }}
-                >
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div className="carrinhoPessoas">
+                <p>Quantidade de pessoas</p>
+                <div>
+                  <button
+                    onClick={(e) => {
+                      minusPessoa();
+                    }}
                   >
-                    <circle cx="10" cy="10" r="9.5" stroke="#252A27" />
-                    <path
-                      d="M6.42773 10H13.0604"
-                      stroke="#252A27"
-                      strokeWidth="1.6"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
-                <span>{pessoas}</span>
-                <button
-                  onClick={(event) => {
-                    plusPessoa();
-                  }}
-                >
-                  <svg
-                    width="21"
-                    height="20"
-                    viewBox="0 0 21 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="10" cy="10" r="9.5" stroke="#252A27" />
+                      <path
+                        d="M6.42773 10H13.0604"
+                        stroke="#252A27"
+                        strokeWidth="1.6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                  <span>{pessoas}</span>
+                  <button
+                    onClick={(event) => {
+                      plusPessoa();
+                    }}
                   >
-                    <circle cx="10.2852" cy="10" r="9.5" stroke="#252A27" />
-                    <path
-                      d="M10.2852 6.70312V13.3358"
-                      stroke="#252A27"
-                      strokeWidth="1.6"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M6.96875 10H13.6014"
-                      stroke="#252A27"
-                      strokeWidth="1.6"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
+                    <svg
+                      width="21"
+                      height="20"
+                      viewBox="0 0 21 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="10.2852" cy="10" r="9.5" stroke="#252A27" />
+                      <path
+                        d="M10.2852 6.70312V13.3358"
+                        stroke="#252A27"
+                        strokeWidth="1.6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M6.96875 10H13.6014"
+                        stroke="#252A27"
+                        strokeWidth="1.6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <p className="carrinhoDesc">{viagem.description}</p>
+              <div className="carrinhoLinks">
+                <Link to="/pacotes">Ver detalhes</Link>
+                <button>
+                  <img src={Lixo} />
                 </button>
               </div>
-            </div>
-            <p className="carrinhoDesc">{viagem.description}</p>
-            <div className="carrinhoLinks">
-              <Link to="/pacotes">Ver detalhes</Link>
-              <button>
-                <img src={Lixo} />
-              </button>
-            </div>
+            </section>
           </section>
         </section>
         <div className="btnPosition">
