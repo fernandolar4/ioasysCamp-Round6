10068bb1a44c9cb362eb997f6c498faa4977380a
Fernando Lara <fmaia8@hotmail.com>
2022-04-21 15:31:07 -0300

    Placeholder paginas incompletas

--- src/pages/Compras/Compras.style.js
+++ src/pages/Compras/Compras.style.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const Desenvolvimento = styled.main`
+  height: 48vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+`;
