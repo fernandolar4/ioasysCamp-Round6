80c914fc698520fe73f7c05e1c7d17a50bac5e9b
Fernando Lara <fmaia8@hotmail.com>
2022-04-21 15:41:44 -0300

    .

--- src/pages/FAQ/FAQ.style.js
+++ src/pages/FAQ/FAQ.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Desenvolvimento = styled.main`
-  height: 48vh;
+  height: 90vh;
   display: flex;
   align-items: center;
   flex-direction: column;
